a7e62444640d87f43e23e7da6bafa3c52bb026b0
Camila <mendezcamilaitati@gmail.com>
2025-11-12 17:29:58 -0300

    Primer commit: estructura base de Spring Boot

---
+++ src/main/java/com/gestionservicios/GestionServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.gestionservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionServiciosApplication.class, args);
+	}
+
+}
